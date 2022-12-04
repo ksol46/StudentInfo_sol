dbb78b541ae10c3b79024c9af54a748ba45f5827
ksol46 <doomee880406@naver.com>
2022-12-05 02:36:44 +0900

    d

--- src/utils/Define.java
+++ src/utils/Define.java
@@ -6,14 +6,12 @@ package utils;
 //상수 : 바뀌지 않는 값.
 public class Define {										//밑에 선언한 값들이 왜 상수이냐!!!!!! final을 붙였기 때문에!!!!!!!
 	//과목코드
-	public static final int KOREA = 1001; //->국어
-	public static final int MATH = 2001; //->수학
+	public static final int KOREA = 1001;
+	public static final int MATH = 1002;
 	
 	//학점 산출 방식 : 타입으로 정할 것이고, 상수는 뭐라고 쓰던 상관없지만 각각 달라야 한다.
-	public static final int AB_TYPE = 0; //A,B .. 일반과목 산출 방식
-	public static final int SAB_TYPE = 1; //S,A,B .. 필수과목 산출 방식
-	
-	
+	public static final int AB_TYPE = 0;
+	public static final int SAB_TYPE = 1;
 	
 	
 	
